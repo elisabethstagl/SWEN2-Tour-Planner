83b0e7252caa4e5dbc3cf91ed8ea616ff387756c
Elisabeth-Stagl <if24b138@technikum-wien.at>
2026-02-20 12:47:17 +0100

    added Spring Boot

---
+++ tourplanner-backend/src/test/java/at/fhtw/tourplanner_backend/TourplannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.tourplanner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourplannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
